df3dcc4adfb5c46339eb5dd261d97a805a7329e0
Christophe Desoutter <christophe.desoutter@airbus.com>
2024-12-25 21:00:23 +0100

    day25

--- day25.py
+++ day25.py
@@ -23,10 +23,47 @@ except:
 data = open(INPUT).readlines()
 data = [l.strip() for l in data]
 
+keys=[]
+locks=[]
 
+i=0
+ldata=len(data)
+while i<ldata:
+    l=data[i]
+    if l == '.....':
+        #key
+        key=[0,0,0,0,0]
+        for j in range(1,6):
+            for e,c in enumerate(data[i+j]): 
+                key[e]+=int(data[i+j][e] == '#')
+        keys.append(key)
 
-#part 1
+    elif l == '#####':
+        #lock
+        lock=[0,0,0,0,0]
+        for j in range(1,6):
+            for e,c in enumerate(data[i+j]): 
+                lock[e]+=int(data[i+j][e] == '#')
+        locks.append(lock)
+    else:
+        raise Exception('Bad input')
+    
+    i+=8
+
+print(f"{keys}")
+print(f"{locks}")
+
+#part 1 count keys matching locks
 res=0
+for l in locks:
+    for k in keys:
+        match=True
+        for i in range(5):
+            if k[i]+l[i]>5:
+                match=False
+                break
+        if match:
+            res+=1
 
 print(f"[SOLVED] - First star: {res}")
 
